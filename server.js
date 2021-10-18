3b104340b866f688a5362f19338a37cfbd88188b
jjangjae <nick2866@naver.com>
2021-10-18 17:12:48 +0900

    장재원 초기셋팅 완료 stage branch 배포

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+const usersRouter = require("./routes/users");
+const cors = require("cors");
+
+//react 연결지워야할 것
+// app.use("/", (req, res) => {
+//     return res.send(express.static(path.join(__dirname, "client/index.html")));
+//   });
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+//routing
+app.use("/api", usersRouter);
+
+//Error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
